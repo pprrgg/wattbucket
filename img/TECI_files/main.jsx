8aac3af6b70b1df72047dc401b424b1ea03781e5
Your Name <you@example.com>
2025-05-21 13:35:27 +0200

    Updates

---
+++ img/TECI_files/main.jsx
@@ -0,0 +1,12 @@
+import __vite__cjsImport0_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=7b53d0bd"; const _jsxDEV = __vite__cjsImport0_react_jsxDevRuntime["jsxDEV"];
+import __vite__cjsImport1_react from "/node_modules/.vite/deps/react.js?v=7b53d0bd"; const StrictMode = __vite__cjsImport1_react["StrictMode"];
+import __vite__cjsImport2_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=7b53d0bd"; const createRoot = __vite__cjsImport2_reactDom_client["createRoot"];
+// import './index.css'
+import App from "/src/App.jsx";
+createRoot(document.getElementById('root')).render(/*#__PURE__*/ _jsxDEV(App, {}, void 0, false, {
+    fileName: "/home/pk/Desktop/app/calculadoradeahorroenergetico_f/src/main.jsx",
+    lineNumber: 9,
+    columnNumber: 3
+}, this));
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanN4Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IFN0cmljdE1vZGUgfSBmcm9tICdyZWFjdCdcbmltcG9ydCB7IGNyZWF0ZVJvb3QgfSBmcm9tICdyZWFjdC1kb20vY2xpZW50J1xuLy8gaW1wb3J0ICcuL2luZGV4LmNzcydcbmltcG9ydCBBcHAgZnJvbSAnLi9BcHAuanN4J1xuXG5cblxuY3JlYXRlUm9vdChkb2N1bWVudC5nZXRFbGVtZW50QnlJZCgncm9vdCcpKS5yZW5kZXIoXG4gIDxBcHAgLz5cbilcbiJdLCJuYW1lcyI6WyJTdHJpY3RNb2RlIiwiY3JlYXRlUm9vdCIsIkFwcCIsImRvY3VtZW50IiwiZ2V0RWxlbWVudEJ5SWQiLCJyZW5kZXIiXSwibWFwcGluZ3MiOiI7QUFBQSxTQUFTQSxVQUFVLFFBQVEsUUFBTztBQUNsQyxTQUFTQyxVQUFVLFFBQVEsbUJBQWtCO0FBQzdDLHVCQUF1QjtBQUN2QixPQUFPQyxTQUFTLFlBQVc7QUFJM0JELFdBQVdFLFNBQVNDLGNBQWMsQ0FBQyxTQUFTQyxNQUFNLGVBQ2hELFFBQUNIIn0=
